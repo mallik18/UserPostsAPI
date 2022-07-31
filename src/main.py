5de127aa0e5897888ce60c2ca1cb444e5d64b8a2
mallik18 <mallikarjunkolar18@gmail.com>
2022-07-31 21:39:09 +0530

    update main file with update endpoint and added comment

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 """ Modules """
-from fastapi import FastAPI, status, HTTPException, Response
+import logging
+import os
+from fastapi import FastAPI, status, HTTPException
 import psycopg2
 from psycopg2.extras import RealDictCursor
-import logging
 
+from dotenv import load_dotenv
 from src.schemas import Posts
 
+load_dotenv()
 
 app = FastAPI()
 
@@ -14,25 +17,27 @@ logging.basicConfig(filename='src/logs/database.log',
 while True:
 
     try:
-        conn = psycopg2.connect(host='localhost', dbname='UserPostsAPI',
-                                user='postgres', password='changed#',
+        conn = psycopg2.connect(host=os.getenv('HOST'), dbname=os.getenv('DBNAME'),
+                                user=os.getenv('USER'), password=os.getenv('PASSWORD'),
                                 cursor_factory=RealDictCursor)
         cursor = conn.cursor()
         print("Database connection was successfull!")
         logging.info("Database connection was successfull!")
         break
-    except BaseException as err:
+    except psycopg2.Error as err:
         print("Connecting to database failed", err)
         logging.error(err)
 
 
 @app.get("/")
 async def root():
+    """ Main entry point"""
     return {"message": "Hello World"}
 
 
 @app.get("/posts")
 def get_posts():
+    """ Get all posts from database"""
     cursor.execute(""" SELECT * FROM posts """)
     posts = cursor.fetchall()
     print(posts)
@@ -41,6 +46,7 @@ def get_posts():
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(new_post: Posts):
+    """Create a new post and insert it into the database"""
     # returning * = Feature of Postgres database to return the query result
 
     cursor.execute(""" INSERT INTO posts(title, content, published)
@@ -51,38 +57,47 @@ def create_posts(new_post: Posts):
     return {"Inserted post detail": ret_post}
 
 
-@app.get("/posts/{id}")
-def get_posts_by_id(id: int):
+@app.get("/posts/{post_id}")
+def get_posts_by_id(post_id: int):
+    """"Get posts by post_id from database"""
     # above id is kept to int because if it is str then there may be SQL injections
     # afterwards we can convert it to str
 
-    cursor.execute(""" SELECT * FROM posts WHERE id= %s """, (str(id), ))
+    cursor.execute(""" SELECT * FROM posts WHERE id= %s """, (str(post_id), ))
     ret_post = cursor.fetchone()
 
     if ret_post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Post with Id {id} was not found")
+                            detail=f"Post with Id {post_id} was not found")
 
     return {"Requested post by id": ret_post}
 
 
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_posts(id: int):
+@app.delete("/posts/{post_id}")
+def delete_posts(post_id: int):
+    """ Delete a post from the database"""
     cursor.execute(""" DELETE FROM posts WHERE id = %s RETURNING * """,
-                       (str(id),))
+                       (str(post_id),))
     deleted_post = cursor.fetchone()
     conn.commit()
 
     if deleted_post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Post with Id {id} does not exist!")
+                            detail=f"Post with Id {post_id} does not exist!")
 
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+    return {"content":f"Post of id {post_id} deleted successfully"}
 
-@app.put("/posts/{id}")
-def update_post(id: int, post: Posts):
-
-    cursor.execute(""" UPDATE posts SET title = %s, content = %s, published = %s, 
-                        returning * """, (post.title, post.content, post.published))
+@app.put("/posts/{post_id}")
+def update_post(post_id: int, post: Posts):
+    """ Update a post """
+    cursor.execute(""" UPDATE posts SET title = %s, content = %s, published = %s
+                       WHERE id = %s RETURNING * """,
+                       (post.title, post.content, post.published, str(post_id)))
     updated_post = cursor.fetchone()
+    conn.commit()
 
+    if update_post is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with Id {post_id} does not exist!")
+
+    return {"UpdatedPost": updated_post}
--- src/schemas.py
+++ src/schemas.py
@@ -1,7 +1,8 @@
+""" Modules"""
 from pydantic import BaseModel
 
-
 class Posts(BaseModel):
+    """ Scheme for Posts"""
     title: str
     content: str
     published: bool = True
