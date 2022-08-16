4a13d50b504b0c5f056ef5e90bb23f7ed81274c4
mallik18 <mallikarjunkolar18@gmail.com>
2022-08-16 16:04:53 +0530

    made some changed requirements file

--- src/models.py
+++ src/models.py
@@ -3,6 +3,7 @@ from sqlalchemy import Column, Integer, String, Boolean
 
 from src.database import Base
 
+
 class Post(Base):
     """ Scheme of Posts table in database"""
     __tablename__ = 'posts'
@@ -11,3 +12,4 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='True', nullable=False)
+    #date_created = Column(TIMESTAMP, nullable=False)
