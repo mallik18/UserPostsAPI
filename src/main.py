8e64d3dc2628bdbb60e4f7a4d67b0b4d8468cf34
mallik18 <mallikarjunkolar18@gmail.com>
2022-08-21 15:42:24 +0530

    clean and removed unwanted comments

--- src/main.py
+++ src/main.py
@@ -7,12 +7,12 @@ from psycopg2.extras import RealDictCursor
 from sqlalchemy.orm import Session
 
 from dotenv import load_dotenv
-from src.schemas import Posts
-from . import models
+from . import models, schemas
 from .database import engine, get_db
 
 load_dotenv()
 
+# this below line
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -43,57 +43,39 @@ async def root():
 @app.get("/posts")
 def get_posts(db_posts: Session= Depends(get_db)):
     """ Get all posts from database"""
-    # cursor.execute(""" SELECT * FROM posts """)
-    # posts = cursor.fetchall()
+
     posts = db_posts.query(models.Post).all()
-    return {"data": posts}
+    return posts
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(new_post: Posts, db_posts: Session= Depends(get_db)):
+def create_posts(new_post: schemas.Posts, db_posts: Session= Depends(get_db)):
     """Create a new post and insert it into the database"""
-    # returning * = Feature of Postgres database to return the query result
-
-    # cursor.execute(""" INSERT INTO posts(title, content, published)
-    #                    VALUES (%s, %s, %s) RETURNING * """,
-    #                     (new_post.title, new_post.content, new_post.published))
-    # ret_post = cursor.fetchone()
-    # conn.commit()
-
-    #ret_post = models.Post(title=new_post.title, content=new_post.content,
-    #                       published=new_post.published)
 
     ret_post = models.Post(**new_post.dict())
     db_posts.add(ret_post)
     db_posts.commit()
     db_posts.refresh(ret_post)
-    return {"Inserted post detail": ret_post}
+
+    return ret_post
 
 
 @app.get("/posts/{post_id}")
 def get_posts_by_id(post_id: int, db_posts: Session= Depends(get_db)):
     """"Get posts by post_id from database"""
-    # above id is kept to int because if it is str then there may be SQL injections
-    # afterwards we can convert it to str
 
-    # cursor.execute(""" SELECT * FROM posts WHERE id= %s """, (str(post_id), ))
-    # ret_post = cursor.fetchone()
     ret_post = db_posts.query(models.Post).filter(models.Post.id == post_id).first()
 
     if ret_post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post with Id {post_id} was not found")
 
-    return {"Requested post by id": ret_post}
+    return ret_post
 
 
 @app.delete("/posts/{post_id}")
 def delete_posts(post_id: int, db_posts: Session= Depends(get_db)):
     """ Delete a post from the database"""
-    # cursor.execute(""" DELETE FROM posts WHERE id = %s RETURNING * """,
-    #                    (str(post_id),))
-    # deleted_post = cursor.fetchone()
-    # conn.commit()
 
     ret_post = db_posts.query(models.Post).filter(models.Post.id == post_id)
 
@@ -107,13 +89,10 @@ def delete_posts(post_id: int, db_posts: Session= Depends(get_db)):
     return {"content":f"Post of id {post_id} deleted successfully"}
 
 @app.put("/posts/{post_id}")
-def update_post(post_id: int, updated_post: Posts, db_posts: Session= Depends(get_db)):
+def update_post(post_id: int, updated_post: schemas.Posts,
+                    db_posts: Session= Depends(get_db)):
     """ Update a post """
-    # cursor.execute(""" UPDATE posts SET title = %s, content = %s, published = %s
-    #                    WHERE id = %s RETURNING * """,
-    #                    (post.title, post.content, post.published, str(post_id)))
-    # updated_post = cursor.fetchone()
-    # conn.commit()
+
     post_query = db_posts.query(models.Post).filter(models.Post.id == post_id)
     post = post_query.first()
 
@@ -124,4 +103,4 @@ def update_post(post_id: int, updated_post: Posts, db_posts: Session= Depends(ge
     post_query.update(updated_post.dict(), synchronize_session=False)
     db_posts.commit()
 
-    return {"UpdatedPost": post_query.first()}
+    return post_query.first()
